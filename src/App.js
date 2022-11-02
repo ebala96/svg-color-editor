28b43a26dcbfe774e4615c4daddb9612aa8474cc
balai-egnaroinc <balai@egnaroinc.com>
2022-11-02 12:53:26 +0530

    Added svg color stroke and fill.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import { useState } from "react";
 import "./styles.css";
+import SvgColorChanger from "./components/Svg-Color-Changer";
 
 const App = () => {
-  const [selectedFile, setSelectedFile] = useState("");
+  const [selectedFile, setSelectedFile] = useState();
+  const [colorForSVG, setColorForSVG] = useState({fill: 'yellow', stroke: "black"})
 
   const changeHandler = (event) => {
     setSelectedFile(event.target.files[0]);
     console.log(event.target.files[0]);
     console.log(selectedFile);
+    setColorForSVG({fill:'red',stroke:'blue'});
   };
 
   return (
     <div className="App">
-      <h2>Upload SVG File</h2>
-      <input type="file" name="file" onChange={changeHandler} />
+        <h2>Upload SVG File</h2>
+        <input type="file" name="file" onChange={changeHandler} />
+        <SvgColorChanger
+            fill = {colorForSVG.fill}
+            stroke = {colorForSVG.stroke}
+        />
+      <div>
+
+      </div>
     </div>
   );
 };
